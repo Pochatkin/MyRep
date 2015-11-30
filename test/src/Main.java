de43c1dea718d6de811b009deaa04ed69b7f313d
Pochatkin <m.a.pochatkin@gmail.com>
2015-11-30 14:02:23 +0300

    kek

---
+++ test/src/Main.java
@@ -0,0 +1,13 @@
+
+/**
+ * Created by Михаил on 29.11.2015.
+ */
+public class Main {
+    public static final String APP_PREFERENCES = "ChatSettings";
+    public static final String APP_PREFERENCES_COUNTER = "counter";
+    private SharedPreferences mSettings;
+
+    public static void main(String[] args) {
+
+    }
+}
